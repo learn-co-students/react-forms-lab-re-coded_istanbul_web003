162e9c7409720b769960735a382d2f100722459f
khaldoon khaldi <khaldoon9kh9@gmail.com>
2020-09-26 13:20:07 +0300

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -3,23 +3,47 @@ import React from "react";
 class LoginForm extends React.Component {
   constructor() {
     super();
-
-    this.state = {};
+    this.state = {
+      username: null,
+      password: null
+    };
   }
+
+  handleInputChange(event) {
+    this.setState({
+      username: event.target.value,
+      password: event.target.value
+    })
+  }
+
+  handleOnSubmit(event){
+    event.preventDefault()
+    if (this.state.username===null){
+      alert("Please fill both feilds") 
+    }else{
+      return this.props.handleLogin
+    }
+  }
+
+
 
   render() {
     return (
-      <form>
+      <form onSubmit={event => this.handleOnSubmit(event)} >
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text"
+            value={this.state.username}
+            onChange={event => this.handleInputChange(event)} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password"
+            value={this.state.password}
+            onChange={event => this.handleInputChange(event)} />
           </label>
         </div>
         <div>
